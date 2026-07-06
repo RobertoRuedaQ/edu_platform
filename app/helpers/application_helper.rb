f4f5d9d4f527b059e5986f1b9623b56c5b071e7a
Roberto Rueda <rueda.roberto89@gmail.com>
2026-07-06 11:04:40 -0500

    stylesheets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  APP_NAME = "EduPlatform".freeze
+
+  # Page title: "<page> · EduPlatform", falling back to the app name alone.
+  # Set the page part with `<% content_for :title, "Estudiantes" %>` in a view.
+  def page_title
+    page = content_for(:title)
+    page.present? ? "#{page} · #{APP_NAME}" : APP_NAME
+  end
 end
